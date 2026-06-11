18d69c61199ff01ebf2c03f5a7b8f3ff158fdeee
Paul Maillard <pmaillard27@proton.me>
2026-06-11 20:04:20 +0200

    amélioration du style

--- controllers/Admin/LinesController.php
+++ controllers/Admin/LinesController.php
@@ -13,13 +13,14 @@ use Models\LinesModel;
 #[RequirePrivilege(Privilege::ADMIN)]
 class LinesController extends Controller
 {
-    public function get(): void
-    {
-        require_once 'models/LinesModel.php';
-        $model = new \Models\LinesModel();
-        
-        $this->data['lines'] = $model->getLinesWithDetails();
-        
-        $this->render();
-    }
+  public function get(): void
+  {
+    require_once 'models/LinesModel.php';
+    $model = new LinesModel();
+
+    $this->data['lines'] = $model->getLinesWithDetails();
+
+    $this->render();
+  }
 }
+
